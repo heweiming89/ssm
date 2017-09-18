140619620fb748b1319bd658c7eaaad24b8820d9
heweiming <heweiming89@163.com>
2017-09-18 11:06:39 +0800

    修改正过时的引用

--- src/main/java/cn/heweiming/ssm/web/json/serializer/XssStringJsonSerializer.java
+++ src/main/java/cn/heweiming/ssm/web/json/serializer/XssStringJsonSerializer.java
@@ -1,24 +1,23 @@
 package cn.heweiming.ssm.web.json.serializer;
 
-import java.io.IOException;
-
-import org.apache.commons.lang3.StringEscapeUtils;
-
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import org.apache.commons.text.StringEscapeUtils;
+
+import java.io.IOException;
 
 public class XssStringJsonSerializer extends JsonSerializer<String> {
 
-	@Override
-	public void serialize(String value, JsonGenerator gen, SerializerProvider serializers)
-			throws IOException, JsonProcessingException {
-		if (value != null) {
-			gen.writeString(StringEscapeUtils.escapeHtml4(value));
-		} else {
-			gen.writeString("");
-		}
-	}
+    @Override
+    public void serialize(String value, JsonGenerator gen, SerializerProvider serializers)
+            throws IOException, JsonProcessingException {
+        if (value != null) {
+            gen.writeString(StringEscapeUtils.escapeHtml4(value));
+        } else {
+            gen.writeString("");
+        }
+    }
 
 }
