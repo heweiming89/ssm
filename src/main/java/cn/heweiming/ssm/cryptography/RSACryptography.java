0ad00e27b67011c09533649f220c7623e03d26bc
heweiming <heweiming89@163.com>
2017-09-18 16:32:33 +0800

    RSACryptography

---
+++ src/main/java/cn/heweiming/ssm/cryptography/RSACryptography.java
@@ -0,0 +1,88 @@
+package cn.heweiming.ssm.cryptography;
+
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public final class RSACryptography {
+
+    private static final String ALGORITHM = "RSA";
+
+    private static KeyFactory keyFactory = null;
+
+    private synchronized static KeyFactory getKeyFactory() throws NoSuchAlgorithmException {
+        if (keyFactory == null) {
+            keyFactory = KeyFactory.getInstance(ALGORITHM);
+        }
+        return keyFactory;
+    }
+
+    public static KeyPair genKeyPair(int keyLength) throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(ALGORITHM);
+        keyPairGenerator.initialize(keyLength);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        return keyPair;
+    }
+
+    public static PublicKey getPublickey(String publicKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] keyBytes = Base64.getDecoder().decode(publicKey.getBytes());
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = getKeyFactory();
+        return keyFactory.generatePublic(keySpec);
+    }
+
+    public static PublicKey getPublicKey(BigInteger modulus, BigInteger exponent) throws Exception {
+        RSAPublicKeySpec publicKeySpec = new RSAPublicKeySpec(modulus, exponent);
+        KeyFactory keyFactory = getKeyFactory();
+        return keyFactory.generatePublic(publicKeySpec);
+    }
+
+    public static PrivateKey getPrivateKey(String privateKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] keyBytes = Base64.getDecoder().decode(privateKey.getBytes());
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = getKeyFactory();
+        return keyFactory.generatePrivate(keySpec);
+    }
+
+    public static byte[] encrypt(byte[] plaintextBytes, PublicKey publicKey) throws NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return cipher.doFinal(plaintextBytes);
+    }
+
+    public static byte[] encrypt(byte[] plaintextBytes, String publicKey)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException,
+            BadPaddingException, InvalidKeySpecException {
+        return encrypt(plaintextBytes, getPublickey(publicKey));
+    }
+
+    public static byte[] decrypt(byte[] ciphertextBytes, PrivateKey privateKey) throws NoSuchAlgorithmException,
+            NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        return cipher.doFinal(ciphertextBytes);
+    }
+
+    public static byte[] decrypt(byte[] ciphertextBytes, String privateKey)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException,
+            InvalidKeyException, InvalidKeySpecException {
+        return decrypt(ciphertextBytes, getPrivateKey(privateKey));
+    }
+
+}
