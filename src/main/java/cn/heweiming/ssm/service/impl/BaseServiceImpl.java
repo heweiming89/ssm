dcd9907a9ba9d3c12b393313d6d61d088e5fa7ae
czheweiming89 <>
2017-09-22 23:58:24 +0800

    BaseServiceImpl

--- src/main/java/cn/heweiming/ssm/service/impl/BaseServiceImpl.java
+++ src/main/java/cn/heweiming/ssm/service/impl/BaseServiceImpl.java
@@ -1,13 +1,16 @@
 package cn.heweiming.ssm.service.impl;
 
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import cn.heweiming.ssm.mapper.BaseMapper;
 import cn.heweiming.ssm.service.BaseService;
-import org.apache.ibatis.session.RowBounds;
-
-import java.util.List;
 
 public class BaseServiceImpl<R, E, M extends BaseMapper<R, E>> implements BaseService<R, E> {
 
+	@Autowired
     protected M mapper;
 
     @Override
